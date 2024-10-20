7a8061cefde1df4a4e40fce9dd0ba60a89bbcf97
Salveh <164516108+Salveh@users.noreply.github.com>
2024-10-20 10:03:57 +0800

    Add files via upload

---
+++ Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace Hub.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string JobTitle { get; set; }
+        public string Address { get; set; }
+        public decimal Salary { get; set; }
+        public int Age { get; set; }
+    }
+}
+
